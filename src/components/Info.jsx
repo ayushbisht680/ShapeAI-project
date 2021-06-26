a802b92245d95adf5e0a93ffbd42badfd381f1a0
Ayush Bisht <83755687+ayushbisht680@users.noreply.github.com>
2021-06-26 18:49:36 +0530

    Add files via upload

---
+++ src/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info(){
+    return (
+    <div className="note">
+        <h1>JavaScript And ReactJs</h1>
+        <p>A basic web development ReactJS bootcamp</p>
+    </div>
+    );  
+    
+}
+
+export default Info;
